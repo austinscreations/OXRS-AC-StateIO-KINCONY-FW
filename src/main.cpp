af5ad34a98e34f4cd75eb77f715a0d7550d94ed5
Austin's Creations <1mcneilaus@gmail.com>
2022-11-13 19:22:26 -0500

    setup i2c bus

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,9 @@
 /*--------------------------- Libraries -------------------------------*/
 #include <Arduino.h>
 #include <Wire.h>                   // For I2C
+#include <Adafruit_PCF8575.h>       // for I/O handling
+#include <OXRS_Input.h>             // For input handling
+#include <OXRS_Output.h>              // For output handling
 #include <PubSubClient.h>           // For MQTT
 #include <OXRS_MQTT.h>              // For MQTT
 #include <OXRS_API.h>               // For REST API
@@ -44,15 +47,31 @@
 #endif
 
 /*--------------------------- Constants ----------------------------------*/
+// TWO i2c buses
+TwoWire Wire  = TwoWire(0);
+TwoWire Wire1 = TwoWire(1);
+
 // Serial
 #define SERIAL_BAUD_RATE            115200
 
 // REST API
 #define REST_API_PORT               80
 
-// TWO i2c buses
-TwoWire I2Cone = TwoWire(0);
-TwoWire I2Ctwo = TwoWire(1);
+// Each CPF8575 has 16 I/O pins
+#define PCF_PIN_COUNT               16
+
+// Set false for breakout boards with external pull-ups
+#define PCF_INTERNAL_PULLUPS        true
+
+// Internal constant used when input type parsing fails
+#define INVALID_INPUT_TYPE          99
+
+// Internal constants used when output type parsing fails
+#define       INVALID_OUTPUT_TYPE   99
+
+// Can have up to 8x PCF8575 on a single I2C bus
+const byte    PCF_I2C_ADDRESS[]     = { 0x24, 0x25, 0x21, 0x22, 0x26, 0x27, 0x20, 0x23 };
+const uint8_t PCF_COUNT             = sizeof(PCF_I2C_ADDRESS);
 
 // Ethernet
 #if defined(ETHMODE)
@@ -73,9 +92,29 @@ TwoWire I2Ctwo = TwoWire(1);
 
 
 /*--------------------------- Global Variables ---------------------------*/
+// OUTPUTS - Each bit corresponds to an PCF found on the I2C bus
+uint8_t g_pcfs_found_do = 0;
 
+// INPUTS - Each bit corresponds to an PCF found on the I2C bus
+uint8_t g_pcfs_found_di = 0;
+
+// How many pins on each MCP are we controlling (defaults to all 16)
+// Set via "outputsPerMcp" integer config option - should be set via
+// the REST API so it is persisted to SPIFFS and loaded early enough
+// in the boot sequence to configure the LCD and adoption payloads
+uint8_t g_pcf_output_pins = PCF_PIN_COUNT;
 
 /*--------------------------- Instantiate Global Objects -----------------*/
+// I/O buffers
+Adafruit_PCF8575 pcf8575_DO[PCF_COUNT]; // Output - wire bus 0
+Adafruit_PCF8575 pcf8575_DI[PCF_COUNT]; // input - wire bus 1
+
+// Input handlers
+OXRS_Input oxrsInput[PCF_COUNT];
+
+// Output handlers
+OXRS_Output oxrsOutput[PCF_COUNT];
+
 #if defined(ETHMODE)
 WiFiClient client;
 WiFiServer server(REST_API_PORT);
@@ -379,14 +418,111 @@ void initialiseSerial()
   Serial.begin(SERIAL_BAUD_RATE);
   delay(1000);
   
-  logger.println(F("[stio ] starting up..."));
+  logger.println(F("[stio] starting up..."));
 
   DynamicJsonDocument json(128);
   getFirmwareJson(json.as<JsonVariant>());
 
-  logger.print(F("[stio ] "));
+  logger.print(F("[stio] "));
   serializeJson(json, logger);
   logger.println();
+}
+
+/*--------------------------- Event Handler -------------------------------*/
+void inputEvent(uint8_t id, uint8_t input, uint8_t type, uint8_t state)
+{
+  // Determine the index for this input event (1-based)
+  uint8_t pcf = id;
+  uint8_t index = (PCF_PIN_COUNT * pcf) + input + 1;
+
+  // Publish the event
+  // publishEvent(index, type, state);
+}
+
+void outputEvent(uint8_t id, uint8_t output, uint8_t type, uint8_t state)
+{
+  // Determine the index (1-based)
+  uint8_t pcf = id;
+  uint8_t pin = output;
+  uint8_t raw_index = (g_pcf_output_pins * pcf) + pin;
+  uint8_t index = raw_index + 1;
+  
+  // Update the MCP pin - i.e. turn the relay on/off (LOW/HIGH)
+  pcf8575_DO[pcf].digitalWrite(pin, state);
+
+  // Publish the event
+  // publishEvent(index, type, state);
+}
+
+/*--------------------------- I2C -------------------------------*/
+void scanI2CBus()
+{
+  logger.println(F("[stio] scanning for output buffers..."));
+
+  for (uint8_t pcf1 = 0; pcf1 < PCF_COUNT; pcf1++)
+  {
+    logger.print(F(" - 0x"));
+    logger.print(PCF_I2C_ADDRESS[pcf1], HEX);
+    logger.print(F("..."));
+
+    // Check if there is anything responding on this address
+    Wire.beginTransmission(PCF_I2C_ADDRESS[pcf1]);
+    if (Wire.endTransmission() == 0)
+    {
+      bitWrite(g_pcfs_found_do, pcf1, 1);
+
+      // If an MCP23017 was found then initialise and configure the outputs
+      pcf8575_DO[pcf1].begin(PCF_I2C_ADDRESS[pcf1],&Wire);
+      for (uint8_t pin = 0; pin < PCF_PIN_COUNT; pin++)
+      {
+        pcf8575_DO[pcf1].pinMode(pin, OUTPUT);
+        pcf8575_DO[pcf1].digitalWrite(pin, RELAY_OFF);
+      }
+
+      // Initialise output handlers
+      oxrsOutput[pcf1].begin(outputEvent, RELAY);
+      
+      logger.println(F("PCF8575"));
+    }
+    else
+    {
+      logger.println(F("empty"));
+    }
+  }
+
+  logger.println(F("[stio] scanning for input buffers..."));
+
+  for (uint8_t pcf2 = 0; pcf2 < PCF_COUNT; pcf2++)
+  {
+    logger.print(F(" - 0x"));
+    logger.print(PCF_I2C_ADDRESS[pcf2], HEX);
+    logger.print(F("..."));
+
+    // Check if there is anything responding on this address
+    Wire1.beginTransmission(PCF_I2C_ADDRESS[pcf2]);
+    if (Wire1.endTransmission() == 0)
+    {
+      bitWrite(g_pcfs_found_di, pcf2, 1);
+      
+      // If an MCP23017 was found then initialise and configure the inputs
+      pcf8575_DI[pcf2].begin(PCF_I2C_ADDRESS[pcf2],&Wire1);
+      for (uint8_t pin = 0; pin < PCF_PIN_COUNT; pin++)
+      {
+        pcf8575_DI[pcf2].pinMode(pin, PCF_INTERNAL_PULLUPS ? INPUT_PULLUP : INPUT);
+      }
+
+      // Initialise input handlers (default to SWITCH)
+      oxrsInput[pcf2].begin(inputEvent, SWITCH);
+
+      logger.print(F("PCF8575"));
+      if (PCF_INTERNAL_PULLUPS) { logger.print(F(" (internal pullups)")); }
+      logger.println();
+    }
+    else
+    {
+      logger.println(F("empty"));
+    }
+  }
 }
 
 /*--------------------------- Program -------------------------------*/
@@ -396,8 +532,11 @@ void setup()
   initialiseSerial();  
 
   // Start the I2C bus
-  I2Cone.begin(I2C_SDA, I2C_SCL);
-  I2Ctwo.begin(I2C_SDA2, I2C_SCL2);
+  Wire.begin(I2C_SDA, I2C_SCL);
+  Wire1.begin(I2C_SDA2, I2C_SCL2);
+
+  // Scan the I2C bus and set up I/O buffers
+  scanI2CBus();
 
   // Set up network/MQTT/REST API
   #if defined(WIFIMODE)
